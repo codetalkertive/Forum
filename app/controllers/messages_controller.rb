93e149e192cdc68da020beea5af75ab597fe3c25
codetalkertive <codetalkertive@gmail.com>
2020-06-28 22:33:34 +0100

    Add Model, Controller, Show ,Create , Edit and Delete Message

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,48 @@
+class MessagesController < ApplicationController
+  def index
+  end
+
+  def new
+  	@message = Message.new
+  end
+
+  def create
+  	@message = Message.create(message_params)
+  	if @message.save
+  		redirect_to @message
+  	else
+  		render :new
+  	end
+  end
+
+  def show
+  	@message = Message.find(params[:id])
+  end
+
+  def edit
+  	@message = Message.find(params[:id])
+  end
+
+  def update
+  	@message = Message.find(params[:id])
+
+  	if @message.update(message_params)
+  		redirect_to @message
+  	else
+  		render :edit
+  	end
+  end
+
+  def destroy
+  	@message = Message.find(params[:id])
+
+  	@message.destroy
+  	redirect_to :action => :index
+  end
+
+  private
+
+  def message_params
+  	params.require(:message).permit(:title, :description)
+  end
+end
