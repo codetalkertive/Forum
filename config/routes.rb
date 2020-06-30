79bbd300072ba6753925beaf4fe8d695eac89a19
codetalkertive <codetalkertive@gmail.com>
2020-06-30 16:12:13 +0100

    Add Styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
+	devise_scope :user do  
+   get '/users/sign_out' => 'devise/sessions#destroy'     
+end
   
   devise_for :users
-  
+
   resources :messages
   root 'messages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
